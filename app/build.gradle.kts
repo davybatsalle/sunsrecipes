a4dae345e4fe5334f8f125396c12679e52e10d5d
Davy Batsalle <dbatsalle@amadeus.com>
2026-09-20 17:39:20 +0200

    Prepare release 1.0.4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,8 +50,8 @@ android {
         applicationId = "com.sunrecipes.app"
         minSdk = 26
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.0.2"
+        versionCode = 2
+        versionName = "1.0.4"
     }
 
     buildFeatures {
